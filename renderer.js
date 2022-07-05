aeeb71f91f72d5ec36351fee9356ec4037771349
PhyXTGears <phyxtgearspro@gmail.com>
2022-07-05 18:24:03 -0400

    Remove Console.log spam around drawAllNodes()
    - Emmie

--- renderer.js
+++ renderer.js
@@ -918,16 +918,12 @@ function drawAllNodes(rootSomething) {
   const moveConditionSwitch = document.createElement("div");
   moveConditionSwitch.classList.add('o-command-moveSwitch');
 
-  console.log("Root elem pre-removal child count: ", rootElement.children.length);
-
   const childList = Array.prototype.slice.call(rootElement.children, 0);
 
   for (let child of childList) {
     rootElement.removeChild(child);
   }
 
-  console.log("Root elem post-removal child count: ", rootElement.children.length);
-  console.log("Root elem: ", rootElement);
   if (moveCondition === "go") {
     moveConditionSwitch.textContent = "Go";
     moveConditionSwitch.classList.remove("o-command-moveswitch-stop");
@@ -942,7 +938,6 @@ function drawAllNodes(rootSomething) {
 
   rootElement.appendChild(moveConditionSwitch);
   rootElement.appendChild(elem);
-  console.log("Root elem post-addition child count: ", rootElement.children.length);
 }
 
 function drawNodes(node) {
