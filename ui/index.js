ad6696fea773215486568663b2ea479bcfc41fa6
Justin Charette <charetjc@gmail.com>
2023-01-18 02:24:25 -0500

    Refactor canvasViewport into module
    - Justin C

--- ui/index.js
+++ ui/index.js
@@ -10,6 +10,8 @@
 import Point from './js/geom/point.js';
 import Vector from './js/geom/vector.js';
 
+import Viewport from './js/viewport.js';
+
 const { open, save } = window.__TAURI__.dialog;
 const { exists, readTextFile, writeTextFile } = window.__TAURI__.fs;
 const { documentDir } = window.__TAURI__.path;
@@ -179,73 +181,7 @@ const commandImages = new Map();
 
 const ZOOM_FACTOR = 1.2;
 
-const canvasViewport = {
-  offset: Vector(0, 0),
-  scale: 1.0,
-
-  panVec: Vector(0, 0),
-
-  zoomIn(pt) {
-    this.zoom(pt, this.scale * (1.0 / ZOOM_FACTOR));
-  },
-
-  zoomOut(pt) {
-    this.zoom(pt, this.scale * (ZOOM_FACTOR / 1.0));
-  },
-
-  zoom(unitPt, scale) {
-    const prevScale = this.scale;
-
-    // ua :: Vector
-    // let ua be vector to anchor pt from origin of unit coordinate system.
-    const ua = unitPt.sub(Point(0, 0));
-
-    // ub1 :: Vector
-    // let ub1 be vector to corner of previous-scaled origin in unit coordinate system.
-    const ub1 = this.offset;
-
-    // uc :: Vector
-    // let uc be vector to corner of previous-scaled origin from anchor in unit coordinate system.
-    const uc = ub1.sub(ua);
-
-    // ucc :: Vector
-    // while uc is in unit coordinate system, it's length matches that of vector in previous-scale coordinate system.
-    // let ucc be vector uc scaled from previous-scale to unit scale.
-    const ucc = uc.scale(1.0 / prevScale);
-
-    // ub2:: Vector
-    // let ub2 be vector (in unit coordinate system) from origin to new-scale corner/origin.
-    const ub2 = ua.add(ucc.scale(scale));
-
-    this.offset = ub2;
-    this.scale = scale;
-  },
-
-  startPan(offset) {
-    this.panVec = offset;
-  },
-
-  pan(offset) {
-    this.offset = this.offset.add(offset.sub(this.panVec));
-    this.panVec = offset;
-  },
-
-  resetZoom() {
-    this.zoom = 1.0;
-  },
-
-  resetPan() {
-    this.offset = Vector(0.0, 0.0);
-  },
-
-  toViewCoord(vec) {
-    return vec.sub(this.offset).scale(1.0 / this.scale);
-  },
-
-  toUnitCoord(vec) {
-    return vec.scale(this.scale).add(this.offset);
-  },
-};
+const canvasViewport = Viewport(ZOOM_FACTOR);
 
 const config = {
   imageFiles: [
